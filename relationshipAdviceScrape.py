07de53cfb8fd7d499268e4e8ae0f5c2955ec78a1
Nik Kelly <nikkelly@outlook.com>
2019-11-23 00:32:52 -0800

    11-23

--- relationshipAdviceScrape.py
+++ relationshipAdviceScrape.py
@@ -1,65 +1,42 @@
 # TODO: Illustrate M v F age ranges 
-# TODO: Figure out how to drop rows with blank ageGenders
 # TODO: How to split 23m into 23 and m from [23m] 
-# Test Regex here: https://www.regextester.com/21
-# Regex Tries: 
-# 23
-# 23m 
-# [23]
-# [23m]
-# [23 m]
-# [23 M]
-# [23M]
-# 23f
-# [23]
-# [23f]
-# [23 f]
-# [23 f]
-# [23f]
 
 import praw
 import pandas as pd
 import re
 from re import search
+from creds_ import USERNAME, PASSWORD, CLIENT_ID, CLIENT_SECRET, USER_AGENT
 
-reddit = praw.Reddit(client_id='K0KrmkKEnFy9Nw',client_secret='GyPJvZsBH-HQgHzY16PkwXL1E_M',user_agent='relationshipScrape',username='relationshipscrape',password='plottingarelationship')
+reddit = praw.Reddit(client_id=CLIENT_ID,client_secret=CLIENT_SECRET,user_agent=USER_AGENT,username=USERNAME,password=PASSWORD)
 
 subreddit = reddit.subreddit('relationships')
 
-post_dict = { 'title':[],\
+post_dict = {   'title':[],\
                 'url': [],\
-                'bracket': [],\
-                'parenth':[],\
+                'ageGenderBracket': [],\
+                'ageGenderParenth': [],\
+                'age': [],\
+                'gender': []
                 }
 
-#Brackets
-regexBracket = re.compile(r'\[(.*?)\]') #! Working
-regexParenth = re.compile(r'\((.*?)\)') #! Working
-regexAge = re.compile(r'\d')
-regexGender = re.compile(r'[a-zA-Z]')
-
+n = 0
 for submission in subreddit.top(limit=100):
     if search('update', submission.title.lower()):
         pass
-    else:
+    elif (search('\[', submission.title)) or (search('\(', submission.title)):
         post_dict['title'].append(submission.title)
         post_dict['url'].append(submission.url)
-        post_dict['bracket'].append(re.findall(regexBracket,submission.title))
-        post_dict['parenth'].append(re.findall(regexParenth, submission.title))
+        #TODO: might need a loop to go through all matches and put them in seperate rows
+        post_dict['ageGenderBracket'].append(re.findall(r'\[(.*?)\]', submission.title))
+        post_dict['ageGenderParenth'].append(re.findall(r'\((.*?)\)',submission.title))
+        post_dict['age'].append(re.findall(r'\d+', str(post_dict['ageGender'][n]))) 
+        post_dict['gender'].append(re.findall(r'[mfMF]', str(post_dict['ageGender'][n]))) 
+        n += 1
+else:
+        pass
 
-post_dictdf = pd.DataFrame(post_dict)
-post_dictdf['ageGender'] = post_dictdf['bracket'] + post_dictdf['parenth']
+post_df = pd.DataFrame(post_dict)
+#save to CSV to debug
+post_df.to_csv('FILENAME.csv', index=False)
 
-# Delete unused columns
-post_dictdf.pop('bracket')
-post_dictdf.pop('parenth')
-#save to CSV 
-post_dictdf.to_csv('FILENAME.csv', index=False)
-
-#TODO
-for index,rows in enumerate(post_dictdf.ageGender):
-    if len(rows) == 0:
-        print(index,rows)
-        print(post_dictdf['title'][index])
-
-post_dictdf.to_csv('testfile.csv', index=False)
+print(post_df['age'][8] + post_df['gender'][8])
